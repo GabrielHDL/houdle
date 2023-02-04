e6d53114015dcdbd9bbe76a585c46503f6401700
Gabriel91017 <47323206+Gabriel91017@users.noreply.github.com>
2023-02-04 14:41:29 -0600

    Update database.php

--- config/database.php
+++ config/database.php
@@ -66,7 +66,7 @@ return [
         'mysql2' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB2_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB2_DATABASE', 'forge'),
             'username' => env('DB2_USERNAME', 'forge'),
